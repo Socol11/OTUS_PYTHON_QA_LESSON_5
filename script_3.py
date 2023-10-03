a63ee67cebd8c33dc2d43f1f88e1f7c7210bd966
Roman <dumgym@gmail.com>
2023-10-03 07:06:31 +0300

    Update script_3.py

--- script_3.py
+++ script_3.py
@@ -2,14 +2,19 @@ import json
 import csv
 
 
-def read_users(file):
-    """Чтение файла с пользователями"""
-    with open(file, 'r') as us:
-        users = json.load(us)
-        return users
+USERS_JSON = 'users.json'
+BOOKS_CSV = 'books.csv'
+RESULT_3_JSON = 'result_3.json'
 
 
-def create_users_list(users):
+def read_from_json(json_file: str) -> dict:
+    """Чтение json-файлов"""
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        return data
+
+
+def create_users_list(users: dict) -> list:
     """Создание нового списка пользователей (список словарей) с четырьмя параметрами для каждого"""
     users_list = list()
     for user in users:
@@ -23,15 +28,15 @@ def create_users_list(users):
     return users_list
 
 
-def read_books(file):
+def read_books(csv_file: str) -> list:
     """Чтение файла с книгами и создание списка книг с параметрами каждой книги (список словарей)"""
-    f = open('books.csv')
-    books = csv.reader(f)
-    next(books)    # Пропускаем заголовок с названиями столбцов
-    return books
+    with open(csv_file) as f:
+        books = csv.reader(f)
+        next(books)    # Пропускаем заголовок с названиями столбцов
+        return list(books)
 
 
-def create_books_list(books):
+def create_books_list(books: list) -> list:
     """Создание списка книг - список словарей с параметрами книг"""
     books_list = list()
     for i in books:
@@ -44,9 +49,9 @@ def create_books_list(books):
     return books_list
 
 
-def create_result_users_list(users_list, chosen_books):
+def create_result_users_list(users_list: list, books_list: list) -> list:
     """Собираем итоговый массив (распределение книг между пользователями)"""
-    books = iter(chosen_books)    # Сначала делаем генератор из списка словарей с книгами
+    books = iter(books_list)    # Сначала делаем генератор из списка словарей с книгами
     while True:    # Многократно проходим по списку пользователей, распределяя по одной книге за итерацию
         try:
             for user in users_list:
@@ -56,25 +61,17 @@ def create_result_users_list(users_list, chosen_books):
     return users_list
 
 
-def write_result_file(users_list):
+def write_result_file(users_list: list):
     """Записываем итоговый результат в файл .json"""
-    with open('result_3.json', 'w') as f:
+    with open(RESULT_3_JSON, 'w') as f:
         json.dump(users_list, f, default=str)
 
 
-def read_result_file(file):
-    """Читаем записанный результат для проверки"""
-    with open('result_3.json', 'r') as res:
-        result = json.load(res)
-        return result
-
-
-read_users = read_users('users.json')
-create_users_list = create_users_list(read_users)
-read_books = read_books('books.csv')
-create_books_list = create_books_list(read_books)
-create_result_users_list = create_result_users_list(create_users_list, create_books_list)
-write_result_file(create_result_users_list)
-
-# Сравниваем результаты работы разных вариантов кода (получились идентичными)
-assert read_result_file('result_1.json') == read_result_file('result_2.json') == read_result_file('result_3.json')
+if __name__ == "__main__":
+    read_users = read_from_json(USERS_JSON)
+    create_users_list = create_users_list(read_users)
+    read_books = read_books(BOOKS_CSV)
+    create_books_list = create_books_list(read_books)
+    create_result_users_list = create_result_users_list(create_users_list, create_books_list)
+    write_result_file(create_result_users_list)
+    read_from_json(RESULT_3_JSON)
